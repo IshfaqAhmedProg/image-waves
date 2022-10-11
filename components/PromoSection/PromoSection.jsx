4a9a765493249aa267b1966d67dd8468aca54208
IshfaqAhmedProg <manslayeriam@gmail.com>
2022-10-12 03:31:57 +0600

    Popular Services full complete, LineDrawer built

---
+++ components/PromoSection/PromoSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './PromoSection.module.css';
+const PromoSection = () => {
+  return (
+    <div className={styles.container}>PromoSection</div>
+  )
+}
+
+export default PromoSection
