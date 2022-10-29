a91902a8467825fc7e915bff5820e885659a29e2
IshfaqAhmedProg <manslayeriam@gmail.com>
2022-10-29 21:50:11 +0600

    prev commit fix 1

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -13,7 +13,9 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.container}>
-      <Image src={LogoFull} alt="We Understand your image aesthetics" />
+      <div className={styles.logo}>
+        <Image src={LogoFull} alt="We Understand your image aesthetics" />
+      </div>
       <div className={styles.menu}>
         <Link href="/">Services</Link>
         <Link href="/">Pricing</Link>
@@ -21,7 +23,7 @@ const Footer = () => {
         <Link href="/">Career</Link>
       </div>
       <div className={styles.content}>
-        <div className={styles.menu+" "+styles.services}>
+        <div className={styles.menu + " " + styles.services}>
           <h4>Services We Provide</h4>
           <Link href="/">Glamour Retouching</Link>
           <Link href="/">Background Removal</Link>
@@ -30,22 +32,21 @@ const Footer = () => {
           <Link href="/">Photo Correction</Link>
           <Link href="/">Wedding Photo Editing</Link>
         </div>
-        <div className={styles.menu+" "+styles.address}>
+        <div className={styles.menu + " " + styles.address}>
           <h4>Address</h4>
           Lorem ipsum,
           <br /> dolor sit
           <br /> 1235486 568-986
           <br /> alps
         </div>
-        <div className={styles.menu+" "+styles.payment}>
+        <div className={styles.menu + " " + styles.payment}>
           <h4>We Accept</h4>
           <div className={styles.options}>
-
-          <Image src={VisaLogo} alt="We Accept Visa"/>
-          <Image src={MastercardLogo} alt="We Accept Mastercard"/>
-          <Image src={MaestroLogo} alt="We Accept Maestro"/>
-          <Image src={AmexLogo} alt="We Accept Amex"/>
-          <Image src={PayPalLogo} alt="We Accept PayPal"/>
+            <Image src={VisaLogo} alt="We Accept Visa" />
+            <Image src={MastercardLogo} alt="We Accept Mastercard" />
+            <Image src={MaestroLogo} alt="We Accept Maestro" />
+            <Image src={AmexLogo} alt="We Accept Amex" />
+            <Image src={PayPalLogo} alt="We Accept PayPal" />
           </div>
         </div>
       </div>
