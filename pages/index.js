7aa56cd06868db8e62cbcbec47362d98fcaf1ba6
IshfaqAhmedProg <manslayeriam@gmail.com>
2022-10-30 05:45:01 +0600

    Prev commit fix 2

--- pages/index.js
+++ pages/index.js
@@ -9,42 +9,28 @@ import FAQ from "../components/IndexComponents/FAQ/FAQ";
 import PopularServices from "../components/IndexComponents/PopularServices/PopularServices";
 import PromoSection from "../components/IndexComponents/PromoSection/PromoSection";
 import ServicesCarousel from "../components/IndexComponents/ServicesCarousel/ServicesCarousel"; */
-import Footer from "../components/Footer/Footer";
-
+const PopularServices = dynamic(() =>
+  import("../components/IndexComponents/PopularServices/PopularServices")
+);
+const PromoSection = dynamic(() =>
+  import("../components/IndexComponents/PromoSection/PromoSection")
+);
+const ServicesCarousel = dynamic(() =>
+  import("../components/IndexComponents/ServicesCarousel/ServicesCarousel")
+);
+const BestDeals = dynamic(() =>
+  import("../components/IndexComponents/BestDeals/BestDeals")
+);
+const OrderSteps = dynamic(() =>
+  import("../components/IndexComponents/OrderSteps/OrderSteps")
+);
+const FAQ = dynamic(() => import("../components/IndexComponents/FAQ/FAQ"));
+const Banner = dynamic(() =>
+  import("../components/IndexComponents/Banner/Banner")
+);
+const Footer = dynamic(() => import("../components/Footer/Footer"));
 export default function Home() {
-  const PopularServices = dynamic(
-    () =>
-      import("../components/IndexComponents/PopularServices/PopularServices"),
-    { ssr: false }
-  );
-  const PromoSection = dynamic(
-    () => import("../components/IndexComponents/PromoSection/PromoSection"),
-    { ssr: false }
-  );
-  const ServicesCarousel = dynamic(
-    () =>
-      import("../components/IndexComponents/ServicesCarousel/ServicesCarousel"),
-    { ssr: false }
-  );
-  const BestDeals = dynamic(
-    () => import("../components/IndexComponents/BestDeals/BestDeals"),
-    { ssr: false }
-  );
-  const OrderSteps = dynamic(
-    () => import("../components/IndexComponents/OrderSteps/OrderSteps"),
-    { ssr: false }
-  );
-  const FAQ = dynamic(() => import("../components/IndexComponents/FAQ/FAQ"), {
-    ssr: false,
-  });
-  const Banner = dynamic(
-    () => import("../components/IndexComponents/Banner/Banner"),
-    { ssr: false }
-  );
-  const Footer = dynamic(
-    () => import("../components/Footer/Footer"),
-    { ssr: false }
-  );
+  //TODO fix code Splitting
   return (
     <>
       <Head>
@@ -56,41 +42,27 @@ export default function Home() {
         <section>
           <Hero></Hero>
         </section>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section>
-            <PopularServices />
-          </section>
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section>
-            <PromoSection />
-          </section>
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section>
-            <ServicesCarousel />
-          </section>
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section>
-            <BestDeals />
-          </section>
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section>
-            <OrderSteps />
-          </section>
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section>
-            <FAQ />
-          </section>
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section>
-            <Banner />
-          </section>
-        </Suspense>
+        <section>
+          <PopularServices />
+        </section>
+        <section>
+          <PromoSection />
+        </section>
+        <section>
+          <ServicesCarousel />
+        </section>
+        <section>
+          <BestDeals />
+        </section>
+        <section>
+          <OrderSteps />
+        </section>
+        <section>
+          <FAQ />
+        </section>
+        <section>
+          <Banner />
+        </section>
       </main>
       <Footer />
     </>
